29c29570086568c146de8ea6e7fc35e833c1287e
jun515 <jun515jun@hotmail.com>
2019-04-02 04:28:15 +0000

    arrayMap2

--- arrayMap.js
+++ arrayMap.js
@@ -7,17 +7,20 @@ const Animals = [
 	{name: 'Jimmy', species: 'fish'}
 ]
 
+const map1 = array1.map(x => x * 2);
 
-let arrayFilter = function(array) {
-    let filteredArray = []  //array to store filtered values
+console.log(map1);
+
+// let arrayFilter = function(array) {
+//     let filteredArray = []  //array to store filtered values
     
-    for (let i = 0; i < array.length; i++){ //loop through each item in passeed array
-        if (array[i].species === 'dog'){    //condition to check if species is a dog
-            filteredArray.push(array[i].name)    //if dog save to array
-        }
-    }
+//     for (let i = 0; i < array.length; i++){ //loop through each item in passeed array
+//         if (array[i].species === 'dog'){    //condition to check if species is a dog
+//             filteredArray.push(array[i].name)    //if dog save to array
+//         }
+//     }
      
-    return filteredArray;   //return filtered array of dogs
-}; 
+//     return filteredArray;   //return filtered array of dogs
+// }; 
 
- console.log (arrayFilter(Animals));
+//  console.log (arrayFilter(Animals));
